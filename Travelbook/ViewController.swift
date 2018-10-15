e36ccae6f386420633838cdb8be991b2d843f59b
Milan Struyf <milanstruyf001@gmail.com>
2018-10-15 15:10:29 +0200

    Initial Commit

---
+++ Travelbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelbook
+//
+//  Created by Milan Struyf on 15/10/2018.
+//  Copyright © 2018 Milan Struyf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
